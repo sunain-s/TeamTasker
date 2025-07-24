d040545b1bc6aca48e2fe6863efe8a6f8ac85305
sunain-s <zcabssy@ucl.ac.uk>
2025-07-24 03:10:18 +0100

    SecurityConfig - bcrypt

--- src/main/java/com/teamtasker/repository/UserRepository.java
+++ src/main/java/com/teamtasker/repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.teamtasker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.teamtasker.entity.User;
 import java.util.Optional;
+import com.teamtasker.entity.User;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
---
+++ src/main/java/com/teamtasker/service/UserService.java
@@ -0,0 +1,8 @@
+package com.teamtasker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
