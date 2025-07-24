419913f72125ce8d9c0b15d6c9e04c49b561ac95
sunain-s <zcabssy@ucl.ac.uk>
2025-07-24 03:35:06 +0100

    UserService - registerUser()

--- src/main/java/com/teamtasker/repository/UserRepository.java
+++ src/main/java/com/teamtasker/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.teamtasker.repository;
 
+import com.teamtasker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-import com.teamtasker.entity.User;
-
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
--- src/main/java/com/teamtasker/service/UserService.java
+++ src/main/java/com/teamtasker/service/UserService.java
@@ -1,8 +1,33 @@
 package com.teamtasker.service;
 
+import com.teamtasker.entity.User;
+import com.teamtasker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("Username is already in use");
+        }
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email is already in use");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
