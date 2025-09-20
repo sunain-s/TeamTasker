dd9702e4230a1adfcb06ffe5217dcaa4620d17e6
sunain-s <zcabssy@ucl.ac.uk>
2025-09-20 02:06:20 +0100

    TeamController: createTeam()

--- src/main/java/com/teamtasker/controller/TeamController.java
+++ src/main/java/com/teamtasker/controller/TeamController.java
@@ -5,6 +5,7 @@ import com.teamtasker.entity.Team;
 import com.teamtasker.entity.User;
 import com.teamtasker.service.TeamService;
 import com.teamtasker.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,9 +13,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -49,7 +53,6 @@ public class TeamController {
         model.addAttribute("teams", teams);
         model.addAttribute("currentUser", currUser);
         model.addAttribute("currentFilter", filter);
-
         return "teams/list";
     }
 
@@ -91,8 +94,27 @@ public class TeamController {
         model.addAttribute("memberTeams", memberTeams);
         model.addAttribute("currentUser", currUser);
         model.addAttribute("currentFilter", filter);
-
         return "teams/my-teams";
+    }
+
+    @GetMapping("/create")
+    public String createTeam(@Valid @ModelAttribute("team") Team team,
+                             BindingResult result,
+                             Authentication authentication,
+                             RedirectAttributes redirectAttributes) {
+        if (teamService.isTeamNameTaken(team.getName())) {
+            result.rejectValue("name", "isNameTaken", "Team name already exists");
+        }
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.team", result);
+            redirectAttributes.addFlashAttribute("team", team);
+            return "redirect:/teams/create";
+        }
+
+        User currUser = getCurrentUser(authentication);
+        Team createdTeam = teamService.createTeam(team.getName(), team.getDescription(), currUser);
+        redirectAttributes.addFlashAttribute("createdTeam", "Team: '" + createdTeam.getName() + "' created successfully!");
+        return "redirect:/teams/" + createdTeam.getId();
     }
 
     private User getCurrentUser(Authentication authentication) {
